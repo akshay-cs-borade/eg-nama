f44a7b6c4d39c901037a5d3008da8d4d779855bf
Akshay Borade <Akshay Borade>
2023-04-03 08:09:18 +0530

    Remove Basic url Auth

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
-    http_basic_authenticate_with name: "akshay", password: "akshara"
+    # http_basic_authenticate_with name: "akshay", password: "akshara"
 
     def index
     end    
